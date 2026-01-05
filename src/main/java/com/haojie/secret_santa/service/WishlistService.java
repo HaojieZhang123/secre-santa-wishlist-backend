d66d78bd88daa3c76d61e452fc74e090939b5549
HaojieZhang123 <haojiezhang123@gmail.com>
2026-01-05 16:11:49 +0100

    [ADD] Implement WishlistService with CRUD operations for Wishlist entity

--- src/main/java/com/haojie/secret_santa/service/WishlistService.java
+++ src/main/java/com/haojie/secret_santa/service/WishlistService.java
@@ -1,8 +1,50 @@
 package com.haojie.secret_santa.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.haojie.secret_santa.model.entity.Wishlist;
+import com.haojie.secret_santa.repository.WishlistRepository;
 
 @Service
 public class WishlistService {
 
+    @Autowired
+    private WishlistRepository wishlistRepository;
+
+    public List<Wishlist> findAll() {
+        return wishlistRepository.findAll();
+    }
+
+    public Optional<Wishlist> findById(Integer id) {
+        return wishlistRepository.findById(id);
+    }
+
+    public Wishlist getById(Integer id) {
+        Optional<Wishlist> wishlist = wishlistRepository.findById(id);
+        if (wishlist.isEmpty()) {
+            return null;
+        } else {
+            return wishlist.get();
+        }
+    }
+
+    public Wishlist store(Wishlist wishlist) {
+        return wishlistRepository.save(wishlist);
+    }
+
+    public Wishlist update(Wishlist wishlist) {
+        return wishlistRepository.save(wishlist);
+    }
+
+    public void deleteById(Integer id) {
+        wishlistRepository.deleteById(id);
+    }
+
+    public void delete(Wishlist wishlist) {
+        wishlistRepository.delete(wishlist);
+    }
 }
